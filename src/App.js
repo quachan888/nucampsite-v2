c556e7804a7766f3722ab18084d2daaec035e644
Andy Quach <andy@Andy-Pro-M1.local>
2022-12-10 22:01:01 -0500

    Router Setup

--- src/App.js
+++ src/App.js
@@ -3,13 +3,19 @@ import CampsitesList from './features/campsites/CampsitesList';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
+import { Route, Routes } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import ContactPage from './pages/ContactPage';
 
 function App() {
     return (
         <div className="App">
             <Header />
-            {/* <CampsitesList /> */}
-            <CampsitesDirectoryPage />
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="contact" element={<ContactPage />} />
+                <Route path="directory" element={<CampsitesDirectoryPage />} />
+            </Routes>
             <Footer />
         </div>
     );
--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ import 'bootstrap-social/bootstrap-social.css';
 import 'font-awesome/css/font-awesome.css';
 import 'typeface-lobster';
 import 'typeface-open-sans';
+import { BrowserRouter } from 'react-router-dom';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
@@ -17,11 +18,9 @@ const root = createRoot(container);
 root.render(
     <React.StrictMode>
         <Provider store={store}>
-            <App />
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
         </Provider>
     </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
