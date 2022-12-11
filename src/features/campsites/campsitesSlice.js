b2f14d4a8fdadf51d346a239217adcbde0c027bc
Andy Quach <andy@Andy-Pro-M1.local>
2022-12-10 21:54:57 -0500

    useState

--- src/features/campsites/campsitesSlice.js
+++ src/features/campsites/campsitesSlice.js
@@ -7,3 +7,7 @@ export const selectAllCampsites = () => {
 export const selectRandomCampsite = () => {
     return CAMPSITES[Math.floor(CAMPSITES.length * Math.random())];
 };
+
+export const selectCampsiteById = (id) => {
+    return CAMPSITES.find((campsite) => campsite.id === id);
+};
