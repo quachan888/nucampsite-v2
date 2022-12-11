959733f3adb221c6dd4a0a8ef8631b4e38055000
Andy Quach <andy@Andy-Pro-M1.local>
2022-12-10 20:53:55 -0500

    Week 1 Workshop Assignment

--- src/App.js
+++ src/App.js
@@ -1,19 +1,14 @@
-import React from 'react';
 import './App.css';
-import NucampLogo from './app/assets/img/logo.png';
-import { Container, Navbar, NavbarBrand } from 'reactstrap';
+import CampsitesList from './features/campsites/CampsitesList';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
     return (
         <div className="App">
-            <Navbar dark color="primary" sticky="top" expand="md">
-                <Container>
-                    <NavbarBrand href="/">
-                        <img src={NucampLogo} alt="Nucamp logo" />
-                    </NavbarBrand>
-                </Container>
-            </Navbar>
-            I'm ready
+            <Header />
+            <CampsitesList />
+            <Footer />
         </div>
     );
 }
